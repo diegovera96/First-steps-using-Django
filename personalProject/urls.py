f5141176bb4527e5885140e488a7a3b494cfbb2f
Diego Vera Santis <diego.vera@alunmos.ucn.cl>
2022-05-04 16:07:17 -0400

    feature: Add Person and Pet APIViews

--- personalProject/urls.py
+++ personalProject/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from petclub.views import HelloWorld
+from petclub.views import (HelloWorld, PersonAPIView, PetAPIView)
 
 urlpatterns = [
     ##path('admin/', admin.site.urls),
     path('hi', HelloWorld.as_view(), name='helloworld'),
     path('api-auth/', include('rest_framework.urls')),
+
+    path('pet/', PetAPIView.as_view(), name="pet"),
+    path('person/', PersonAPIView.as_view(), name="person"),
 ]
